0787d39afe5103b5c006f681e9b02192abf04ef9
aman <amanavelon@gmail.com>
2024-02-20 17:49:08 +0530

    login and registerworking

--- app/src/main/java/com/example/amanshopping/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/amanshopping/fragment/LoginFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.amanshopping.databinding.FragmentLoginBinding
+import com.example.amanshopping.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +18,8 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: LoginViewModel by viewModels()
+    private var result:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -29,7 +35,27 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        binding.loginButton.setOnClickListener {
+            val email =binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            viewModel.login(email,password)
+        }
+        viewModel.result.observe(viewLifecycleOwner){
+            if (it.isNotEmpty()){
+                result = it
+                toast(result)
+            }
+        }
+
     }
+
+    private fun toast(result: String?) {
+        Toast.makeText(requireContext(), result, Toast.LENGTH_SHORT)?.show()
+    }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/example/amanshopping/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/amanshopping/fragment/RegisterFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.amanshopping.databinding.FragmentRegisterBinding
+import com.example.amanshopping.uitl.Resource
+import com.example.amanshopping.viewmodel.RegisterViewModel
+import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,26 +18,37 @@ class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val viewModel by viewModels<RegisterViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        _binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.registerButton.setOnClickListener {
+            val username = binding.usernameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            viewModel.createUser(username, email, password)
+        }
+
+        viewModel.response.observe(viewLifecycleOwner) {
+            makeToast(it)
+        }
+    }
+
+    private fun makeToast(s: Resource<FirebaseUser>) {
+        Toast.makeText(requireContext(), s.toString(), Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
-
 }
